38490fae6e5a18785d898e6c311683ed7435d84c
Carl Woodhouse <carl.woodhouse@gmail.com>
2022-11-14 23:55:04 +0000

    moree fixes

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
-const isProd = process.env.NODE_ENV === "production";
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? "/banshee-gg/" : "",
-
   images: {
-    loader: 'imgix',
-    path: 'https://noop',
+    unoptimized: true,
   },
 }
 
