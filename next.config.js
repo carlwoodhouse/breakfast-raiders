68cb413a23031aaf136193960485e07591f4fc7e
Carl Woodhouse <carl.woodhouse@gmail.com>
2022-11-14 23:13:45 +0000

    next config changes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,24 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
-    loader: "imgix",
-    path: "",
+    loader: 'imgix',
+    path: 'https://noop',
   },
 }
 
