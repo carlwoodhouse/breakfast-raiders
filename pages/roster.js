ee6956b81ee86a2d82b2aa46d781e8b1cac12c85
Carl Woodhouse <carl.woodhouse@gmail.com>
2022-12-04 11:05:32 +0000

    if equal sort by ilvl

--- pages/roster.js
+++ pages/roster.js
@@ -36,7 +36,7 @@ export default function Home({ data }) {
 
 export async function getStaticProps ({ query }) {
     const range = await googleSheetsService.getRange('Roster!A3:L50');
-    const data = range.sort((a, b) => a[9] < b[9] ? 1: -1);
+    const data = range.sort((a, b) => a[9] < b[9] || (a[9] == b[9] && a[1] < b[1]) ? 1: -1);
     console.log(data)
 
     return { 
