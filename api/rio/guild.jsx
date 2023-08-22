e190120927189812191631dc9bcd2df0ac05a4b8
Carl Woodhouse <carl.woodhouse@gmail.com>
2023-08-23 00:28:04 +0100

    allow out of guild members, more filtering

--- api/rio/guild.jsx
+++ api/rio/guild.jsx
@@ -1,20 +1,14 @@
+import { csvToArray } from '../../utils';
 import rioCharacter from './character';
 import rioClient from './client'
 
 export default class rioGuild {
   constructor(name, realm, raidRanks, socialRanks, altRanks) {
-
-
-    let additionalMembers = this.additionalMembers();
-
     this.name = name;
     this.realm = realm;
     this.raidRanks = raidRanks;;
     this.socialRanks = socialRanks;
     this.altRanks = altRanks;
-
-
-    // this._additionalMembers = additionalMembers;
   }
 
   async initialize() {
@@ -23,11 +17,14 @@ export default class rioGuild {
     let rg = await client.getGuild(this.name, this.realm);
     let rgMembers = rg.members;
     this.members = await Promise.all(rgMembers.filter(r => this.getAllRanks().includes(r.rank)).map(async x => await rioCharacter.initialize(x.character.name, x.character.realm, x.rank)).filter(x => x != null));
+
+    let additionalMembers = await this.populateAdditionalMembers();
+    this.members = this.members.concat(additionalMembers);
   }
 
   getAllRanks() {
     return this.raidRanks.concat(this.altRanks);
-  // return this.raidRanks.concat(this.socialRanks).concat(this.altRanks);
+    // return this.raidRanks.concat(this.socialRanks).concat(this.altRanks);
   }
 
   getRaiders() {
@@ -46,24 +43,20 @@ export default class rioGuild {
     return this.members;
   }
 
-  additionalMembers() {
-    // const settings = new settings();
-    // const range = settings.AdditionalCharacters;
-    // let members = [];
+  async populateAdditionalMembers() {
+    let addMembers = [];
 
-    // let numRows = range.getNumRows();
+    if (process.env.ADDITIONAL_CHARACTERS != undefined && process.env.ADDITIONAL_CHARACTERS != "") {
+      const charKeys = csvToArray(process.env.ADDITIONAL_CHARACTERS);
 
-    // for (let r = 1; r <= numRows; r += 1) {
-    //     let name = range.getCell(r, 1).getValue();
+      for await (const x of charKeys) {
+        // name|realm|rank
+        let keyParts = csvToArray(x, "|");
+        let member = await rioCharacter.initialize(keyParts[0], keyParts[1], Number(keyParts[2]));
+        addMembers.push(member);
+      }
+    }
 
-    //     if (name === "") { break; }
-
-    //     let rank = range.getCell(r, 3).getValue();
-    //     let realm = range.getCell(r, 2).getValue();
-
-    //     members.push(new rioGuildCharacter(name, rank, realm));
-    // }
-
-    return rioCharacter[0];
+    return addMembers;
   }
 }
