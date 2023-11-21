b17ad895799c09e3455f1cee63b4a864c157a46e
Carl Woodhouse <carl.woodhouse@gmail.com>
2023-11-21 14:30:37 +0000

    fixed alts

--- pages/roster.js
+++ pages/roster.js
@@ -63,7 +63,7 @@ export default function Home({ raiders, lastUpdated }) {
                   <td>{alt.mp_score}</td>
                   <td className='class'>{alt.name}</td>
                   <td>{alt.ilvl}</td>
-                  <td>{user.tierCount}</td>
+                  <td>{alt.tierCount}</td>
                   <td>{alt.mp_myth}</td>
                   <td>{alt.mp_twenties}</td>
                   <td>{alt.mp_max}</td>
